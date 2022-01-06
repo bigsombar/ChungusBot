75ac70ac2c6fd4ad59e94eb4b1d2ff2526164d61
Alexander Sambor <frostsambor@outlook.com>
2022-01-06 11:17:35 +0300

    first commit

---
+++ index.ts
@@ -0,0 +1,24 @@
+import DiscordJS, { Intents } from 'discord.js'
+import dotevn from 'dotenv'
+dotevn.config()
+
+const client = new DiscordJS.Client({
+    intents: [
+       Intents.FLAGS.GUILDS,
+       Intents.FLAGS.GUILD_MESSAGES
+    ]
+})
+
+client.on('ready', () => {
+    console.log('Chungus is ready my ass')
+})
+
+client.on('messageCreate', (message) => {
+    if (message.content === 'ping') {
+        message.reply({
+            content: 'pong',
+        })
+    }
+})
+
+client.login(process.env.TOKEN)
