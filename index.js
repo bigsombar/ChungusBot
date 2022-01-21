4d1434009e7dcaf1e85c1f74e11880fa76338181
Alexander Sambor <frostsambor@outlook.com>
2022-01-21 11:24:07 +0300

    Добавил синхронизацию юзеров с бд

--- index.js
+++ index.js
@@ -33,7 +33,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const discord_js_1 = __importStar(require("discord.js"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const pg_1 = require("pg");
 dotenv_1.default.config();
+// const pool = new Pool({          //-for dev usage
+//     user: 'bigsombar',
+//     host: '192.168.0.180',
+//     database: 'bigsombar',
+//     password: '7015',
+//     port: 5432,
+// })
+const pool = new pg_1.Pool({
+    user: 'bigsombar',
+    host: '127.0.0.1',
+    database: 'bigsombar',
+    password: '7015',
+    port: 5432,
+});
 const client = new discord_js_1.default.Client({
     intents: [
         discord_js_1.Intents.FLAGS.GUILDS,
@@ -54,18 +69,65 @@ client.on('ready', () => {
     else {
         commands = (_a = client.application) === null || _a === void 0 ? void 0 : _a.commands;
     }
+    //REPEAT FUNCTIONS
+    function TestPost() {
+        let channel = guild === null || guild === void 0 ? void 0 : guild.channels.cache.find(c => c.name === 'bot-test');
+        if (channel === null || channel === void 0 ? void 0 : channel.isText()) {
+            channel.send('POG');
+        }
+    }
+    function BDSync() {
+        return __awaiter(this, void 0, void 0, function* () {
+            let dUsers = [
+                {
+                    id: "id",
+                    username: "username",
+                    nickname: "nickname",
+                    avatar_url: "url"
+                }
+            ];
+            yield (guild === null || guild === void 0 ? void 0 : guild.members.fetch().then((members) => members.forEach(member => {
+                let memberNick = member.nickname;
+                if (memberNick == null) {
+                    memberNick = "отсутствует";
+                }
+                dUsers.push({
+                    id: member.id,
+                    username: member.user.username,
+                    nickname: memberNick,
+                    avatar_url: member.user.avatarURL()
+                });
+            })));
+            dUsers.shift();
+            (() => __awaiter(this, void 0, void 0, function* () {
+                const client = yield pool.connect();
+                try {
+                    dUsers.forEach((d) => __awaiter(this, void 0, void 0, function* () {
+                        const res = yield client.query(`
+                    INSERT INTO users (user_id, user_name, user_nickname, user_avatarurl) 
+                    VALUES (${d.id}, '${d.username}', '${d.nickname}', '${d.avatar_url}')  
+                    ON CONFLICT (user_id) DO UPDATE SET (user_name, user_nickname, user_avatarurl) = (EXCLUDED.user_name, EXCLUDED.user_nickname, EXCLUDED.user_avatarurl)`);
+                    }));
+                }
+                finally {
+                    client.release();
+                }
+            }))().catch(err => console.log(err.stack));
+        });
+    }
+    // COMMANDS DECLARATION
     commands === null || commands === void 0 ? void 0 : commands.create({ name: 'ядро',
         description: 'кидает в тебя ядро, берегись',
     });
     commands === null || commands === void 0 ? void 0 : commands.create({ name: 'юзеры',
         description: 'выводит список пользователей сервера',
     });
-    commands === null || commands === void 0 ? void 0 : commands.create({ name: 'удалить_команду',
-        description: 'удаляет указанную слэш команду с сервера',
+    commands === null || commands === void 0 ? void 0 : commands.create({ name: 'юзер',
+        description: 'выводит информацию о пользователе',
         options: [
             {
-                name: 'команда',
-                description: 'введите название команды без /',
+                name: 'имя',
+                description: 'имя пользователя до #',
                 required: true,
                 type: discord_js_1.default.Constants.ApplicationCommandOptionTypes.STRING
             }
@@ -88,9 +150,21 @@ client.on('ready', () => {
             }
         ]
     });
+    commands === null || commands === void 0 ? void 0 : commands.create({ name: 'удалить_команду',
+        description: 'удаляет указанную слэш команду с сервера',
+        options: [
+            {
+                name: 'команда',
+                description: 'введите название команды без /',
+                required: true,
+                type: discord_js_1.default.Constants.ApplicationCommandOptionTypes.STRING
+            }
+        ]
+    });
+    var TestPostInterval = setInterval(BDSync, (60000 * 60)); //выполняется каждый час
 });
 client.on('interactionCreate', (interaction) => __awaiter(void 0, void 0, void 0, function* () {
-    var _a, _b, _c, _d, _e, _f, _g, _h, _j, _k, _l, _m, _o, _p, _q, _r, _s, _t, _u, _v, _w, _x, _y;
+    var _a, _b, _c, _d, _e, _f, _g, _h, _j, _k, _l, _m, _o, _p, _q, _r, _s, _t, _u, _v, _w, _x, _y, _z, _0, _1, _2;
     if (!interaction.isCommand()) {
         return;
     }
@@ -125,10 +199,62 @@ client.on('interactionCreate', (interaction) => __awaiter(void 0, void 0, void 0
             }, 4000);
         }
     }
+    if (commandName === 'юзеры') {
+        let list = [];
+        yield ((_g = interaction.guild) === null || _g === void 0 ? void 0 : _g.members.fetch().then((members) => members.forEach((member) => {
+            list.push(`${member.user.username} | ${member.nickname}`);
+        })));
+        interaction.reply({
+            content: ` зачем тебе эта информация? (¬､¬) \nя удалю это через 15 секунд, записывай быстрее`,
+            ephemeral: false,
+        });
+        setTimeout(() => {
+            interaction.deleteReply();
+        }, 7000);
+        const embedUserList = new discord_js_1.MessageEmbed()
+            .setColor('GREEN')
+            .setTitle('Список пользователей:')
+            .setDescription(list.join("\n"))
+            .setImage('https://c.tenor.com/UZmwl8vaGC0AAAAi/peepo-g.gif');
+        (_h = interaction.channel) === null || _h === void 0 ? void 0 : _h.send({ embeds: [embedUserList] }).then(msg => {
+            setTimeout(() => msg.delete(), 15000);
+        });
+    }
+    if (commandName === 'юзер') {
+        let userName = options.getString('имя');
+        let m = yield ((_j = interaction.guild) === null || _j === void 0 ? void 0 : _j.members.fetch());
+        let user_id = (_k = m === null || m === void 0 ? void 0 : m.find(m => m.user.username === userName)) === null || _k === void 0 ? void 0 : _k.id;
+        let user_nick;
+        let user_avatar;
+        let roles = [];
+        yield ((_m = (_l = interaction.guild) === null || _l === void 0 ? void 0 : _l.members.fetch(user_id)) === null || _m === void 0 ? void 0 : _m.then((member) => {
+            user_nick = member.nickname;
+            user_avatar = member.user.avatarURL();
+            member.roles.cache.each(role => {
+                roles.push(`<@&${role.id}>`);
+            });
+        }));
+        interaction.reply({
+            content: `Хотел подробностей, да?\nно я не могу долго такое показывать`,
+            ephemeral: false,
+        });
+        setTimeout(() => {
+            interaction.deleteReply();
+        }, 7000);
+        const embedUserInfo = new discord_js_1.MessageEmbed()
+            .setColor('GREEN')
+            .setTitle(userName)
+            .setDescription(`${user_nick}`)
+            .setImage(`${user_avatar}`)
+            .addField('Роли:', roles.join('\n'), true);
+        (_o = interaction.channel) === null || _o === void 0 ? void 0 : _o.send({ embeds: [embedUserInfo] }).then(msg => {
+            setTimeout(() => msg.delete(), 20000);
+        });
+    }
     if (commandName === 'кто_я') {
         let commandFunctions = require('./InternalFunctions.js');
-        let adminStatus = (_j = (yield ((_h = (_g = interaction.command) === null || _g === void 0 ? void 0 : _g.guild) === null || _h === void 0 ? void 0 : _h.members.fetch(interaction.user.id)))) === null || _j === void 0 ? void 0 : _j.permissions.has("ADMINISTRATOR");
-        let nick = (_m = (yield ((_l = (_k = interaction.command) === null || _k === void 0 ? void 0 : _k.guild) === null || _l === void 0 ? void 0 : _l.members.fetch(interaction.user.id)))) === null || _m === void 0 ? void 0 : _m.nickname;
+        let adminStatus = (_r = (yield ((_q = (_p = interaction.command) === null || _p === void 0 ? void 0 : _p.guild) === null || _q === void 0 ? void 0 : _q.members.fetch(interaction.user.id)))) === null || _r === void 0 ? void 0 : _r.permissions.has("ADMINISTRATOR");
+        let nick = (_u = (yield ((_t = (_s = interaction.command) === null || _s === void 0 ? void 0 : _s.guild) === null || _t === void 0 ? void 0 : _t.members.fetch(interaction.user.id)))) === null || _u === void 0 ? void 0 : _u.nickname;
         let firstName = options.getString('имя').toLowerCase();
         let dayOfBirth = options.getNumber('день');
         let you = commandFunctions.names(firstName, dayOfBirth);
@@ -155,18 +281,19 @@ client.on('interactionCreate', (interaction) => __awaiter(void 0, void 0, void 0
             (_a = interaction.channel) === null || _a === void 0 ? void 0 : _a.send(`${nick} заявляет, что он теперь ${you}`);
         }, 3000);
         if (!adminStatus) {
-            (_p = (_o = interaction.command) === null || _o === void 0 ? void 0 : _o.guild) === null || _p === void 0 ? void 0 : _p.members.fetch(interaction.user.id).then((member) => { member.setNickname(you); });
+            (_w = (_v = interaction.command) === null || _v === void 0 ? void 0 : _v.guild) === null || _w === void 0 ? void 0 : _w.members.fetch(interaction.user.id).then((member) => { member.setNickname(you); });
         }
     }
     if (commandName === 'удалить_команду') {
         let cmdName = options.getString('команда');
-        let adminStatus = (_s = (yield ((_r = (_q = interaction.command) === null || _q === void 0 ? void 0 : _q.guild) === null || _r === void 0 ? void 0 : _r.members.fetch(interaction.user.id)))) === null || _s === void 0 ? void 0 : _s.permissions.has("ADMINISTRATOR");
+        let adminStatus = (_z = (yield ((_y = (_x = interaction.command) === null || _x === void 0 ? void 0 : _x.guild) === null || _y === void 0 ? void 0 : _y.members.fetch(interaction.user.id)))) === null || _z === void 0 ? void 0 : _z.permissions.has("ADMINISTRATOR");
         if (adminStatus) {
-            let foundCmdName = (_u = (_t = interaction.guild) === null || _t === void 0 ? void 0 : _t.commands.cache.find(c => c.name === cmdName)) === null || _u === void 0 ? void 0 : _u.name;
-            if (foundCmdName !== undefined) {
-                (_w = (_v = interaction.guild) === null || _v === void 0 ? void 0 : _v.commands.cache.find(c => c.name === cmdName)) === null || _w === void 0 ? void 0 : _w.delete();
+            let c = yield ((_0 = interaction.guild) === null || _0 === void 0 ? void 0 : _0.commands.fetch());
+            let foundCmdId = (_1 = c === null || c === void 0 ? void 0 : c.find(c => c.name === cmdName)) === null || _1 === void 0 ? void 0 : _1.id;
+            if (foundCmdId !== undefined) {
+                (_2 = interaction.guild) === null || _2 === void 0 ? void 0 : _2.commands.fetch(foundCmdId).then((com) => { com.delete(); });
                 interaction.reply({
-                    content: `команда ${foundCmdName} удалена`,
+                    content: `команда ${cmdName} удалена`,
                     ephemeral: true,
                 });
             }
@@ -183,27 +310,6 @@ client.on('interactionCreate', (interaction) => __awaiter(void 0, void 0, void 0
                 ephemeral: true,
             });
         }
-    }
-    if (commandName === 'юзеры') {
-        let list = [];
-        yield ((_x = interaction.guild) === null || _x === void 0 ? void 0 : _x.members.fetch().then((members) => members.forEach((member) => {
-            list.push(`${member.user.username} | ${member.nickname}`);
-        })));
-        interaction.reply({
-            content: ` зачем тебе эта информация? (¬､¬) \nя удалю это через 15 секунд, записывай быстрее`,
-            ephemeral: false,
-        });
-        setTimeout(() => {
-            interaction.deleteReply();
-        }, 7000);
-        const embedUserList = new discord_js_1.MessageEmbed()
-            .setColor('GREEN')
-            .setTitle('Список пользователей:')
-            .setDescription(list.join("\n"))
-            .setImage('https://c.tenor.com/UZmwl8vaGC0AAAAi/peepo-g.gif');
-        (_y = interaction.channel) === null || _y === void 0 ? void 0 : _y.send({ embeds: [embedUserList] }).then(msg => {
-            setTimeout(() => msg.delete(), 15000);
-        });
     }
 }));
 client.login(process.env.TOKEN);
--- index.ts
+++ index.ts
@@ -3,11 +3,30 @@
 //git add *, git commit -m "sampletext", git push
 //botsync in cmd
 //добавляй await к запросам требующим время идиот
+//НЕ ЗАБЫТЬ NPM INSTALL и вручную спуллить на сервак
 import { Utils } from 'discord-api-types'
 import DiscordJS, { Channel, Guild, Intents, Message, MessageEmbed } from 'discord.js'
 import { MembershipStates } from 'discord.js/typings/enums'
 import dotevn from 'dotenv'
+import { Pool, Client } from 'pg'
+
 dotevn.config()
+
+// const pool = new Pool({          //-for dev usage
+//     user: 'bigsombar',
+//     host: '192.168.0.180',
+//     database: 'bigsombar',
+//     password: '7015',
+//     port: 5432,
+// })
+const pool = new Pool({          //-for server usage
+    user: 'bigsombar',
+    host: '127.0.0.1',
+    database: 'bigsombar',
+    password: '7015',
+    port: 5432,
+})
+
 
 const client = new DiscordJS.Client({
     intents: [
@@ -30,24 +49,70 @@ client.on('ready', () => {
     } else {
         commands = client.application?.commands
     }
-
+    //REPEAT FUNCTIONS
+    function TestPost() {
+        let channel = guild?.channels.cache.find(c => c.name === 'bot-test')
+        if (channel?.isText()) {
+            channel.send('POG')
+        }
+    }
+    async function BDSync() {
+        let dUsers = [
+            {
+                id: "id",
+                username: "username",
+                nickname: "nickname",
+                avatar_url: "url"
+            }
+        ];
+        await guild?.members
+        .fetch()
+        .then((members) =>
+            members.forEach(member => {
+                let memberNick = member.nickname
+                if(memberNick == null){memberNick = "отсутствует"}
+                dUsers.push({
+                    id: member.id,
+                    username: member.user.username,
+                    nickname: memberNick,
+                    avatar_url: member.user.avatarURL()!
+                })
+            }),
+        );
+        dUsers.shift()
+        ;(async () => {
+            const client = await pool.connect()
+            try {
+                dUsers.forEach(async d => {
+                    const res = await client.query(`
+                    INSERT INTO users (user_id, user_name, user_nickname, user_avatarurl) 
+                    VALUES (${d.id}, '${d.username}', '${d.nickname}', '${d.avatar_url}')  
+                    ON CONFLICT (user_id) DO UPDATE SET (user_name, user_nickname, user_avatarurl) = (EXCLUDED.user_name, EXCLUDED.user_nickname, EXCLUDED.user_avatarurl)`
+                    )
+                })
+            } finally {
+              client.release()
+            }
+          })().catch(err => console.log(err.stack))  
+    }
+    // COMMANDS DECLARATION
     commands?.create({name: 'ядро',
         description: 'кидает в тебя ядро, берегись',
     })
     commands?.create({name: 'юзеры',
     description: 'выводит список пользователей сервера',
     })
-    commands?.create({name: 'удалить_команду',
-        description: 'удаляет указанную слэш команду с сервера',
-        options: [
-            {
-                name: 'команда',
-                description: 'введите название команды без /',
-                required: true,
-                type: DiscordJS.Constants.ApplicationCommandOptionTypes.STRING 
-            }
-        ]
-    })
+    commands?.create({name: 'юзер',
+    description: 'выводит информацию о пользователе',
+    options: [
+        {
+            name: 'имя',
+            description: 'имя пользователя до #',
+            required: true,
+            type: DiscordJS.Constants.ApplicationCommandOptionTypes.STRING
+        }
+    ]
+    })  
     commands?.create({name: 'кто_я',
         description: 'кто ты по жизни?',
         options: [
@@ -65,8 +130,19 @@ client.on('ready', () => {
             }
         ]
     })
+    commands?.create({name: 'удалить_команду',
+    description: 'удаляет указанную слэш команду с сервера',
+    options: [
+        {
+            name: 'команда',
+            description: 'введите название команды без /',
+            required: true,
+            type: DiscordJS.Constants.ApplicationCommandOptionTypes.STRING 
+        }
+    ]
+    }) 
 
-
+    var TestPostInterval = setInterval(BDSync, (60000*60)) //выполняется каждый час
 })
 
 client.on('interactionCreate', async (interaction) => {
@@ -100,7 +176,65 @@ client.on('interactionCreate', async (interaction) => {
             }, 4000);
         }                                
     }
+    if(commandName === 'юзеры') {
+        let list: string[] = []
+        await interaction.guild?.members
+        .fetch()
+        .then((members) =>
+            members.forEach((member) => {
+                list.push(`${member.user.username} | ${member.nickname}`)
+            }),
+        );
+        interaction.reply({
+            content: ` зачем тебе эта информация? (¬､¬) \nя удалю это через 15 секунд, записывай быстрее`,
+            ephemeral: false,
+        })
+        setTimeout(() => {
+            interaction.deleteReply()
+        }, 7000);  
+        const embedUserList = new MessageEmbed()
+        .setColor('GREEN')
+        .setTitle('Список пользователей:')
+        .setDescription(list.join("\n"))
+        .setImage('https://c.tenor.com/UZmwl8vaGC0AAAAi/peepo-g.gif')
+        interaction.channel?.send({ embeds: [embedUserList] })
+        .then(msg => {
+            setTimeout(() => msg.delete(), 15000)
+        })
+    }
+    if(commandName === 'юзер') {
+        let userName = options.getString('имя')!
+        let m = await interaction.guild?.members.fetch()
+        let user_id = m?.find(m => m.user.username === userName)?.id
+        let user_nick
+        let user_avatar
+        let roles: string[] = []
+        await interaction.guild?.members.fetch(user_id!)?.then((member)=> {
+            user_nick = member.nickname
+            user_avatar = member.user.avatarURL()
+            member.roles.cache.each(role => {
+                roles.push(`<@&${role.id}>`)
+            })
+        })
+        interaction.reply({
+            content: `Хотел подробностей, да?\nно я не могу долго такое показывать`,
+            ephemeral: false,
+        })
+        setTimeout(() => {
+            interaction.deleteReply()
+        }, 7000); 
+        const embedUserInfo = new MessageEmbed()
+        .setColor('GREEN')
+        .setTitle(userName)
+        .setDescription(`${user_nick}`)
+        .setImage(`${user_avatar}`)
+        .addField('Роли:', roles.join('\n'), true)       
+        interaction.channel?.send({ embeds: [embedUserInfo] })
+        .then(msg => {
+            setTimeout(() => msg.delete(), 20000)
+        })
 
+    }
     if(commandName === 'кто_я') {
         let commandFunctions = require('./InternalFunctions.js');
         let adminStatus = (await interaction.command?.guild?.members.fetch(interaction.user.id))?.permissions.has("ADMINISTRATOR")
@@ -133,16 +267,16 @@ client.on('interactionCreate', async (interaction) => {
             interaction.command?.guild?.members.fetch(interaction.user.id).then((member)=> {member.setNickname(you)})
         }
     }
-
     if(commandName === 'удалить_команду') {
         let cmdName = options.getString('команда')!
         let adminStatus = (await interaction.command?.guild?.members.fetch(interaction.user.id))?.permissions.has("ADMINISTRATOR")
         if(adminStatus){
-            let foundCmdName = interaction.guild?.commands.cache.find(c => c.name === cmdName)?.name
-            if (foundCmdName !== undefined) {
-                interaction.guild?.commands.cache.find(c => c.name === cmdName)?.delete()
+            let c = await interaction.guild?.commands.fetch()
+            let foundCmdId = c?.find(c => c.name === cmdName)?.id
+            if (foundCmdId !== undefined) {
+                interaction.guild?.commands.fetch(foundCmdId).then((com)=> {com.delete()})
                 interaction.reply({
-                    content: `команда ${foundCmdName} удалена`,
+                    content: `команда ${cmdName} удалена`,
                     ephemeral: true,     
                 }) 
             } else {
@@ -158,35 +292,10 @@ client.on('interactionCreate', async (interaction) => {
             })
         }
     }
+    
+    
 
-    if(commandName === 'юзеры') {
-        let list: string[] = []
-        await interaction.guild?.members
-        .fetch()
-        .then((members) =>
-            members.forEach((member) => {
-                list.push(`${member.user.username} | ${member.nickname}`)
-            }),
-        );
-        interaction.reply({
-            content: ` зачем тебе эта информация? (¬､¬) \nя удалю это через 15 секунд, записывай быстрее`,
-            ephemeral: false,
-        })
-        setTimeout(() => {
-            interaction.deleteReply()
-        }, 7000);  
-        const embedUserList = new MessageEmbed()
-        .setColor('GREEN')
-        .setTitle('Список пользователей:')
-        .setDescription(list.join("\n"))
-        .setImage('https://c.tenor.com/UZmwl8vaGC0AAAAi/peepo-g.gif')
-        interaction.channel?.send({ embeds: [embedUserList] })
-        .then(msg => {
-            setTimeout(() => msg.delete(), 15000)
-        })
-    }
-
-
+    
 })
 
 
